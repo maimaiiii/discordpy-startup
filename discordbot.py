03234694a7a156bec8d2981221aebcc7218f852d
maimaiiii <72211064+maimaiiii@users.noreply.github.com>
2020-10-02 01:32:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -2,20 +2,8 @@ from discord.ext import commands
 import os
 import traceback
 
-bot = commands.Bot(command_prefix='@gori-timer')
+bot = commands.Bot(command_prefix='@gori-timer-v2')
 token = os.environ['DISCORD_BOT_TOKEN']
-
-
-@bot.event
-async def on_message(message):
-    # 「おはよう」で始まるか調べる
-    if message.content.startswith("おはよう"):
-        # 送り主がBotだった場合反応したくないので
-        if bot.user != message.author:
-            # メッセージを書きます
-            m = "おはようございます" + message.author.name + "さん！"
-            # メッセージが送られてきたチャンネルへメッセージを送ります
-            await message.channel.send(m)
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -28,5 +16,16 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+@bot.command()
+async def on_message(message):
+    # 「おはよう」で始まるか調べる
+    if message.content.startswith("おはよう"):
+        # 送り主がBotだった場合反応したくないので
+        if bot.user != message.author:
+            # メッセージを書きます
+            m = "おはようございます" + message.author.name + "さん！"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await ctx.send(m)
 
+    
 bot.run(token)
