d1e2ae88efb8fb2b7892ab7fec7a61d3d90251ce
maimaiiii <72211064+maimaiiii@users.noreply.github.com>
2020-10-02 01:54:59 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,16 +16,4 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
-@bot.command()
-async def on_message(message):
-    # 「おはよう」で始まるか調べる
-    if message.content.startswith("おはよう"):
-        # 送り主がBotだった場合反応したくないので
-        if bot.user != message.author:
-            # メッセージを書きます
-            m = "おはようございます" + message.author.name + "さん！"
-            # メッセージが送られてきたチャンネルへメッセージを送ります
-            await ctx.send(m)
-
-    
 bot.run(token)
