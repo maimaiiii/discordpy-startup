cb0160b3bbcf417d9c16f9bb3ef5f9d587202aae
maimaiiii <72211064+maimaiiii@users.noreply.github.com>
2020-10-02 01:19:08 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,8 +7,15 @@ token = os.environ['DISCORD_BOT_TOKEN']
 
 
 @bot.event
-async def on_ready():
-    print('ログイン成功')
+async def on_message(message):
+    # 「おはよう」で始まるか調べる
+    if message.content.startswith("おはよう"):
+        # 送り主がBotだった場合反応したくないので
+        if bot.user != message.author:
+            # メッセージを書きます
+            m = "おはようございます" + message.author.name + "さん！"
+            # メッセージが送られてきたチャンネルへメッセージを送ります
+            await message.channel.send(m)
 
 @bot.event
 async def on_command_error(ctx, error):
