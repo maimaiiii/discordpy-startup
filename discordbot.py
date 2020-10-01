47e699b1e8594afddfb3218e7ff1030f619082e6
maimaiiii <72211064+maimaiiii@users.noreply.github.com>
2020-10-02 01:01:31 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,13 +5,10 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
-@client.event
+@bot.event
 async def on_ready():
     print('起動中')
 
-
-@bot.event
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
