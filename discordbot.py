b9e3d908ceb6c0921de64542478555d6bf6d7827
maimaiiii <72211064+maimaiiii@users.noreply.github.com>
2020-10-02 02:02:05 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,15 +5,20 @@ import traceback
 bot = commands.Bot(command_prefix='')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
 
+@bot.command()
+async def ゴリラ(ctx):
+    await ctx.send('ゴリラじゃねぇ！ゴリラ人間です。')
+    
+@bot.command()
+async def 休憩(ctx):
+    await ctx.send('お疲れ様！:cake:')
+
+@bot.command()
+async def ウホ(ctx):
+    await ctx.send('はぁはぁはぁ')
+    
 bot.run(token)
